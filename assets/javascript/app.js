64aafa3ee72ac17c208233f194cad689e4c3fe13
Kenichiwaa <kenichiwa.ninja@gmail.com>
2016-10-26 05:34:52 -0700

    timer is working properly, need to work on end game page and start button, css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,16 +47,16 @@ var question2 ={
 	explanation: ["In the beginning, only EV automobiles were in production. It was only after the invention of the alternator, did petrol vehicles become popular."]
 };	
 
-var question2 ={
-	question: "To participate in the The 'Peking to Paris Rally' vintageant category, how old does your car have to be?",
-	rightAnswer: "1941 or older",
-	possibleAnswers: ["1990 or older","SpaceShips Dude Com'on!", "1941 or older", "1975 or older"],
+var question3 ={
+	question: "To participate in the Peking to Paris Rally 'Vintageant' Category, your vehicle must have a manufactured date prior to..",
+	rightAnswer: "1941",
+	possibleAnswers: ["1956","Dude! Spaceships!", "1941", "1975"],
 	image: ["http://vintagebentley.com/wp-content/uploads/p2p-header.jpeg"],
 };	
 
 var question3 ={
 	question: "What is the largest category in the Dakar Rally Race?",
-	rightAnswer: "trucks",
+	rightAnswer: "Trucks",
 	possibleAnswers: ["32 inch Chome Wheels Yo!", "Trucks", "Apache Helicopters", "Tanks"],
 	image: ["https://s-media-cache-ak0.pinimg.com/originals/2e/98/ab/2e98abee4e48319c8bd7332782593990.jpg"],
 };
@@ -76,6 +76,7 @@ var question4 ={
 var questions = [question1, question2, question3];
 var currentQuestion = 0;
 
+var button = '<p><a class="btn btn-primary btn-lg" href="#" role="button">Next</a></p>';
 
 
 // FUNCTIONS (Reusable blocks of code that I will call upon when needed)
@@ -86,6 +87,8 @@ var currentQuestion = 0;
 // 			  .addClass('answersBox'); // Display possible answers
 // };
 
+
+// DISPLAY QUESTIONS ON THE SCREEN
 function questionsList (){
 	// Grab element #answers
 	var answersListOnPage = document.getElementById("answers");
@@ -103,48 +106,104 @@ function questionsList (){
 	}
 };
 
+				  		// $("#answersExplanation").html('<img class="image" src="' + questions[currentQuestion].image + '">')
+				  		// 						.append(questions[currentQuestion].explanation)
+				  		// 						.append(button)
+				  		// 						.addClass("answersExplanation");
+
+
+// SHOWS EXPLANATION TO QUESTION
+function answersExplained(){ 
+	$("#answersExplanation").html('<img class="image" src="' + questions[currentQuestion].image + '">')
+							.append(questions[currentQuestion].explanation)
+							.append(button)
+							.addClass("answersExplanation");
+	currentQuestion++
+	correct++	
+	$(".answers").remove();
+
+	$(".btn").click(function(){
+		startGame(); 
+		$("#answersExplanation").empty();
+		number = 5;
+		counter = setInterval(decrement, 1000);
+	}); 
+};
+
+
 function startGame (){
-if(currentQuestion == questions.length){
-	alert("gameOver!")
-}
-else{
-	questionsList ()
+	if(currentQuestion == questions.length){
+		alert(correct)
+		$("#answersExplanation").html('<p>' + correct + '</p>').append(wrong);
+	}
+	else{
+		questionsList () // Grabs answers to the question 
 
-	// Display Question
-	$('#questions').html(questions[currentQuestion].question);
+			// Display Question
+			$('#questions').html(questions[currentQuestion].question);
 
-		//On clicking an answer
-		$( ".answers" ).click(function() {
-			  $(this).addClass("clickedAnswer");
+			//On clicking an answer
+			$( ".answers" ).click(function() {
+				  $(this).addClass("clickedAnswer");
 
-			  		//If I click the correct answer, 
-				  if( $('.clickedAnswer').html() === questions[currentQuestion].rightAnswer){
-				  		alert("true!");
-				  		currentQuestion++
-				  		correct++
-				  		$(".answers").remove();
-				  		startGame();
-				  }
-				  else{
-				  	alert("wrong!")
-				  	currentQuestion++
-				  	wrong++
-				  	$(".answers").remove();
-				  	startGame();
-				  };
-				  
-		}); //End clicking answers action
+				  	  //If I click the correct answer..
+					  if( $('.clickedAnswer').html() === questions[currentQuestion].rightAnswer){
+					  			answersExplained()
+					  			stop();
+					  			$("#questions").text("Correct!");	
+	 		
+					  } // End if I click the correct answer.
 
-};//If currentQuestion === # of questions, end game
+					  // If I click the wrong answer..
+					  else{  
+					  	answersExplained()
+					  	stop();
+					  	$("#questions").text("Wrong!");	
+					  }; // End else I click the wrong answer 
+					  
+			}); //End clicking answers action
+		
+	};//If currentQuestion === # of questions, end game
 }; //Function for Start Game
 
 
+
+        // Set our number counter to 100.
+        var number = 5;
+
+        function run(){
+            counter = setInterval(decrement, 1000);
+        }
+
+        // The decrement function.
+        function decrement(){
+            // Decrease number by one.
+            number--;
+            // Show the number in the #show-number tag.
+            $('#show-number').html('<h2>' + number + '</h2>');
+
+            // Once number hits zero...
+            if (number === 0){
+                // ...run the stop function.
+                stop();
+				answersExplained(); 	
+            }
+        }
+
+        function stop(){
+        	clearInterval(counter);
+        	$("#questions").text("Time is Up!");
+            // Clears our "counter" interval.
+            // We just pass the name of the interval
+            // to the clearInterval function.
+            //clearInterval(counter);
+        }
 
 // MAIN PROCESS
 // =================================================================
 startGame(); // Start Game
 
-
+run();
 
 
 
